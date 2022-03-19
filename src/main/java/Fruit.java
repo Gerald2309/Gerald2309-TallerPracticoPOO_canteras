168f07c810a5cbab4346901f8bd6b72d2dd12c98
Geraldine <Gerald.e2309@gmail.com>
2022-03-18 23:05:23 -0500

    Creación de clase Fruit

---
+++ src/main/java/Fruit.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Fruit {
+
+    public String name;
+    private float averageWight;
+    public ArrayList<String> colors;
+
+    public ArrayList<String> getColors() {
+        return colors;
+    }
+
+    public void setColors(ArrayList<String> colors) {
+        this.colors = colors;
+    }
+}
