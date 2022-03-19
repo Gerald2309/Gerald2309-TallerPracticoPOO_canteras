1425f893b4c07b400a84d70ad2573704d5437583
Geraldine <Gerald.e2309@gmail.com>
2022-03-18 23:10:04 -0500

    Creación de clase BankAccount

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,13 @@
+public class BankAccount {
+
+    private int accountNumber;
+    protected  boolean activated;
+
+    public boolean getActivated() {
+        return activated;
+    }
+
+    public void setActivated(boolean activated) {
+        this.activated = activated;
+    }
+}
